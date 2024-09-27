c8bacc4dfce09e033c330db3650397f6c036e19a
Eddie James <edward.james@students.plymouth.ac.uk>
2024-09-27 15:06:08 +0100

    First Push
    Changes from lab 1 to be finished in evening :)

--- Tasks/TASK-203-PortOut/main.cpp
+++ Tasks/TASK-203-PortOut/main.cpp
@@ -5,16 +5,20 @@
 #define TRAF_YEL1_PIN PC_3
 #define TRAF_RED1_PIN PC_2
 #define LEDMASK 0b0000000001001100
+#define LEDMASK2 0b0100000010000001
 // Objects
 //BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN);
 PortOut leds(PortC, LEDMASK);
+PortOut leds2(PortB, LEDMASK2);
 
 int main()
 {
     while (true) {
         leds = 0;   //Binary 000
+        leds2 = 0;
         wait_us(500000);
         leds = 0xFF;   //Binary 11111111
+        leds2 = 0xFFFF;
         wait_us(500000);    
     }
 }
--- Tasks/Task-202-BusOut/main.cpp
+++ Tasks/Task-202-BusOut/main.cpp
@@ -9,14 +9,14 @@
 // DigitalOut grn(TRAF_GRN1_PIN);
 // DigitalOut yel(TRAF_YEL1_PIN);
 // DigitalOut red(TRAF_RED1_PIN,1);
-BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN);
+BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN, PB_0, PB_7, PB_14);
 
 int main()
 {
     while (true) {
         leds = 0;   //Binary 000
         wait_us(500000);
-        leds = 7;   //Binary 111
+        leds = 63;   //Binary 111
         wait_us(500000);    
     }
 }
